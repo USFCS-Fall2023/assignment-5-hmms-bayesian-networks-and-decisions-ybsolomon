5a50fc736138ebdb7c7075fee1af917997ec6e0d
ybsolomon <ybsolomon@dons.usfca.edu>
2023-11-03 13:16:04 -0700

    finished with part 2 and 3

--- HMM.py
+++ HMM.py
@@ -5,55 +5,223 @@ import argparse
 import codecs
 import os
 import numpy
+import random
 
-# observations
-class Observation:
-    def __init__(self, stateseq, outputseq):
-        self.stateseq  = stateseq   # sequence of states
-        self.outputseq = outputseq  # sequence of outputs
-    def __str__(self):
-        return ' '.join(self.stateseq)+'\n'+' '.join(self.outputseq)+'\n'
-    def __repr__(self):
-        return self.__str__()
-    def __len__(self):
-        return len(self.outputseq)
 
-# hmm model
+def getObservations(filename):
+    observations = []
+    file = open(filename, 'r')
+    line = file.readline()
+
+    while line:
+        if len(line) > 1:
+            observations.append(['#'] + line.split())
+
+        line = file.readline()
+
+    return observations
+
+
 class HMM:
     def __init__(self, transitions={}, emissions={}):
-        """creates a model from transition and emission probabilities"""
-        ## Both of these are dictionaries of dictionaries. e.g. :
-        # {'#': {'C': 0.814506898514, 'V': 0.185493101486},
-        #  'C': {'C': 0.625840873591, 'V': 0.374159126409},
-        #  'V': {'C': 0.603126993184, 'V': 0.396873006816}}
-
         self.transitions = transitions
         self.emissions = emissions
 
-    ## part 1 - you do this.
     def load(self, basename):
-        """reads HMM structure from transition (basename.trans),
-        and emission (basename.emit) files,
-        as well as the probabilities."""
+        trans = open(basename + '.trans', 'r')
+        line = trans.readline()
+        while line:
+            words = line.split()
+            if not words[0] in self.transitions:
+                self.transitions[words[0]] = {'#': 0.0}
 
+            self.transitions[words[0]][words[1]] = float(words[2])
+            line = trans.readline()
 
+        trans.close()
 
-   ## you do this.
+        self.emissions['#'] = {}
+        emit = open(basename + '.emit', 'r')
+        line = emit.readline()
+        while line:
+            words = line.split()
+            if not words[0] in self.emissions:
+                self.emissions[words[0]] = {}
+
+            self.emissions[words[0]][words[1]] = float(words[2])
+            self.emissions['#'][words[1]] = 0.0
+            line = emit.readline()
+
+        emit.close()
+
     def generate(self, n):
-        """return an n-length observation by randomly sampling from this HMM."""
+        states = ['#']
+        emissions = ['#']
+        current = '#'
 
+        for i in range(n):
+            random_state = random.choices(population=list(self.transitions[current].keys()),
+                                          weights=list(self.transitions[current].values()))[0]
 
+            emission = random.choices(population=list(self.emissions[random_state].keys()),
+                                          weights=list(self.emissions[random_state].values()))[0]
+            states.append(random_state)
+            emissions.append(emission)
 
-    ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
-    ## determine the most likely sequence of states.
+            current = random_state
 
+        return states, emissions
+
+    def forward(self, observation):
+        rows, cols = (len(self.transitions), len(observation))
+        matrix = [[0.0 for i in range(cols)] for j in range(rows)]
+
+        matrix[0][0] = 1.0
+
+        state_num = 0
+        for state in self.transitions.keys():
+            trans = self.transitions[observation[0]]
+
+            if observation[1] not in self.emissions[state]:
+                emission = 0
+            else:
+                emission = self.emissions[state][observation[1]]
+
+            matrix[state_num][1] = trans[state] * emission
+
+            state_num += 1
+
+        for i in range(2, len(observation)):
+            state_num = 0
+            for state in self.transitions.keys():
+                sum = 0
+                s2_num = 0
+                for s2 in self.transitions.keys():
+                    if observation[i] not in self.emissions[state]:
+                        emission_prob = 0
+                    else:
+                        emission_prob = self.emissions[state][observation[i]]
+
+                    transition_prob = self.transitions[s2][state]
+
+                    sum += matrix[s2_num][i-1]*transition_prob*emission_prob
+                    s2_num += 1
+
+                matrix[state_num][i] = sum
+
+                state_num += 1
+
+        final_vals = [row[-1] for row in matrix]
+        states = list(self.transitions.keys())
+        max_val, max_state = final_vals[0], states[0]
+        for i in range(1, len(final_vals)):
+            if max_val < final_vals[i]:
+                max_val = final_vals[i]
+                max_state = states[i]
+
+        return max_state
+
+    # you do this: Implement the Viterbi algorithm. Given an Observation (a list of outputs or emissions)
+    # determine the most likely sequence of states.
     def viterbi(self, observation):
-        """given an observation,
-        find and return the state sequence that generated
-        the output sequence, using the Viterbi algorithm.
-        """
+        rows, cols = (len(self.transitions), len(observation))
+        matrix = [[0.0 for i in range(cols)] for j in range(rows)]
+        back_pointers = [[0 for i in range(cols)] for j in range(rows)]
+
+        matrix[0][0] = 1.0
+
+        state_num = 0
+        for state in self.transitions.keys():
+            trans = self.transitions[observation[0]]
+
+            if observation[1] not in self.emissions[state]:
+                emission = 0
+            else:
+                emission = self.emissions[state][observation[1]]
+
+            matrix[state_num][1] = trans[state] * emission
+
+            state_num += 1
+
+        for i in range(2, len(observation)):
+            state_num = 0
+            for state in self.transitions.keys():
+                s2_num = 0
+                max_val = -1
+                max_idx = 0
+                for s2 in self.transitions.keys():
+                    if observation[i] not in self.emissions[state]:
+                        emission_prob = 0
+                    else:
+                        emission_prob = self.emissions[state][observation[i]]
+
+                    transition_prob = self.transitions[s2][state]
+
+                    curr_val = matrix[s2_num][i - 1] * transition_prob * emission_prob
+                    if curr_val > max_val:
+                        max_val = curr_val
+                        max_idx = s2_num
+
+                    s2_num += 1
+
+                matrix[state_num][i] = max_val
+                back_pointers[state_num][i] = int(max_idx)
+
+                state_num += 1
+
+        final_vals = [row[-1] for row in matrix]
+        states = list(self.transitions.keys())
+        max_val, max_state, max_idx = final_vals[0], states[0], 0
+        for i in range(1, len(final_vals)):
+            if max_val < final_vals[i]:
+                max_val = final_vals[i]
+                max_idx = i
+
+        state_order = []
+        current_idx = max_idx
+
+        for i in reversed(range(1, len(observation))):
+            next_point = back_pointers[current_idx][i]
+
+            state_order.append(states[current_idx])
+            current_idx = next_point
+
+        state_order.reverse()
+
+        return state_order
+
+    def printMatrix(self, matrix):
+        keys = list(self.transitions.keys())
+        for i in range(len(self.transitions)):
+            print(keys[i], matrix[i])
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get word frequencies from a file.")
+    parser.add_argument('file', type=str)
 
+    parser.add_argument('--generate', type=int)
+    parser.add_argument('--forward', type=str)
+    parser.add_argument('--viterbi', type=str)
 
+    args = parser.parse_args()
 
+    model = HMM()
+    model.load(args.file)
+
+    if args.generate:
+        generated = model.generate(args.generate)
+        print(generated[0])
+        print(generated[1])
+
+    if args.forward:
+        observations = getObservations(args.forward)
+        for observation in observations:
+            print(observation)
+            print('predicted', model.forward(observation))
+
+    if args.viterbi:
+        observations = getObservations(args.viterbi)
+        for observation in observations:
+            print(observation)
+            print('predicted', model.viterbi(observation))
--- carnet.py
+++ carnet.py
@@ -1,71 +1,93 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
+from pgmpy.factors.discrete import TabularCPD
 
 car_model = BayesianNetwork(
     [
         ("Battery", "Radio"),
         ("Battery", "Ignition"),
-        ("Ignition","Starts"),
-        ("Gas","Starts"),
-        ("Starts","Moves")
+        ("Ignition", "Starts"),
+        ("Gas", "Starts"),
+        ("KeyPresent", "Starts"),
+        ("Starts", "Moves")
     ]
 )
 
-# Defining the parameters using CPT
-from pgmpy.factors.discrete import TabularCPD
-
 cpd_battery = TabularCPD(
     variable="Battery", variable_card=2, values=[[0.70], [0.30]],
-    state_names={"Battery":['Works',"Doesn't work"]},
+    state_names={"Battery": ['Works', "Doesn't work"]},
+)
+
+cpd_key_present = TabularCPD(
+    variable="KeyPresent", variable_card=2, values=[[0.70],
+                                                     [0.30]],
+    state_names={"KeyPresent": ['yes', "no"]},
 )
 
 cpd_gas = TabularCPD(
     variable="Gas", variable_card=2, values=[[0.40], [0.60]],
-    state_names={"Gas":['Full',"Empty"]},
+    state_names={"Gas": ['Full', "Empty"]},
 )
 
 cpd_radio = TabularCPD(
-    variable=  "Radio", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
+    variable="Radio", variable_card=2,
+    values=[[0.75, 0.01],
+            [0.25, 0.99]],
     evidence=["Battery"],
     evidence_card=[2],
     state_names={"Radio": ["turns on", "Doesn't turn on"],
-                 "Battery": ['Works',"Doesn't work"]}
+                 "Battery": ['Works', "Doesn't work"]}
 )
 
 cpd_ignition = TabularCPD(
-    variable=  "Ignition", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
+    variable="Ignition", variable_card=2,
+    values=[[0.75, 0.01],
+            [0.25, 0.99]],
     evidence=["Battery"],
     evidence_card=[2],
     state_names={"Ignition": ["Works", "Doesn't work"],
-                 "Battery": ['Works',"Doesn't work"]}
+                 "Battery": ['Works', "Doesn't work"]}
 )
 
 cpd_starts = TabularCPD(
     variable="Starts",
     variable_card=2,
-    values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
-    evidence=["Ignition", "Gas"],
-    evidence_card=[2, 2],
-    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
+    values=[[0.99, 0.01, 0.01, 0.01, .01, .01, .01, .01],
+            [0.01, 0.99, 0.99, 0.99, .99, .99, .99, .99]],
+    evidence=["Ignition", "Gas", "KeyPresent"],
+    evidence_card=[2, 2, 2],
+    state_names={"Starts": ['yes', 'no'],
+                 "Ignition": ["Works", "Doesn't work"],
+                 "Gas": ['Full', "Empty"],
+                 "KeyPresent": ['yes', "no"]},
 )
 
 cpd_moves = TabularCPD(
     variable="Moves", variable_card=2,
-    values=[[0.8, 0.01],[0.2, 0.99]],
+    values=[[0.8, 0.01],
+            [0.2, 0.99]],
     evidence=["Starts"],
     evidence_card=[2],
     state_names={"Moves": ["yes", "no"],
-                 "Starts": ['yes', 'no'] }
+                 "Starts": ['yes', 'no']}
 )
 
 
 # Associating the parameters with the model structure
-car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+car_model.add_cpds(cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key_present)
 
 car_infer = VariableElimination(car_model)
 
-print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+# print(car_infer.query(variables=["Battery"], evidence={"Moves": "no"}))
+#
+# print(car_infer.query(variables=["Starts"], evidence={"Radio": "Doesn't turn on"}))
+#
+# print(car_infer.query(variables=["Radio"], evidence={"Battery": "Works", "Gas": "Full"}))
+# print(car_infer.query(variables=["Radio"], evidence={"Battery": "Works"}))
+#
+# print(car_infer.query(variables=["Ignition"], evidence={"Moves": "no"}))
+# print(car_infer.query(variables=["Ignition"], evidence={"Moves": "no", "Gas": "Empty"}))
 
+# print(car_infer.query(variables=["Starts"], evidence={"Radio": "turns on", "Gas": "Full"}))
 
+print(car_infer.query(variables=["Starts"], evidence={"KeyPresent": "no", "Ignition": "Works", "Gas": "Empty"}))
